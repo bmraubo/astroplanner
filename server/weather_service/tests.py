2fc3079f37060fa370510f9b122655f3f8434dcf
bmraubo <bmraubo@gmail.com>
2022-04-27 15:14:11 +0100

    fetch weather data

--- server/weather_service/tests.py
+++ server/weather_service/tests.py
@@ -5,6 +5,10 @@ from weather_service.weather_service import WeatherService
 class TestWeatherService(TestCase):
 
     def test_weather_service_can_fetch_weather_data(self):
+        location = {"lat": 0.00, "lon": 0.00}
         weather_service = WeatherService()
-        weather_data = weather_service.fetch_weather_data()
+        weather_data = weather_service.fetch_weather_data(location)
+        print(weather_data)
         self.assertTrue(type(weather_data) == dict)
+        self.assertTrue("lat" in weather_data.keys())
+        self.assertTrue("lon" in weather_data.keys())
--- server/weather_service/weather_service.py
+++ server/weather_service/weather_service.py
@@ -1,4 +1,16 @@
+from server.main.settings import OPEN_WEATHER_API_KEY
+import requests
+
 class WeatherService:
     
-    def fetch_weather_data(self):
-        return {}
+    def fetch_weather_data(self, location):
+        request_url = self.build_request_url(location)
+        response = self.make_request(request_url)
+        return response.json()
+
+    def build_request_url(self, location):
+        return f"https://api.openweathermap.org/data/2.5/onecall?lat={location['lat']}&lon={location['lon']}&exclude=hourly,daily&appid={OPEN_WEATHER_API_KEY}"
+    
+    def make_request(self, request_url):
+        response = requests.get(request_url)
+        return response
