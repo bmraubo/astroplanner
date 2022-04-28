b4dd128137295634aef6a181dcf07e56efa83443
bmraubo <bmraubo@gmail.com>
2022-04-28 12:16:30 +0100

    extract current relevant data

--- server/weather_service/tests.py
+++ server/weather_service/tests.py
@@ -13,3 +13,12 @@ class TestWeatherService(TestCase):
         self.assertEqual(weather_data["lat"], location["lat"])
         self.assertTrue("lon" in weather_data.keys())
         self.assertEqual(weather_data["lon"], location["lon"])
+
+    def test_weather_service_can_extract_cloud_cover_data(self):
+        location = {"lat": 10.00, "lon": 20.00}
+        weather_service = WeatherService(location)
+        weather_data = weather_service.fetch_weather_data()
+        relevant_data = weather_service.extract_relevant_data(weather_data)
+        self.assertTrue("clouds" in relevant_data["current"].keys())
+        self.assertTrue("visibility" in relevant_data["current"].keys())
+        self.assertTrue("temp" in relevant_data["current"].keys())
--- server/weather_service/weather_service.py
+++ server/weather_service/weather_service.py
@@ -3,12 +3,23 @@ import requests
 
 class WeatherService:
     base_url = "https://api.openweathermap.org/data/2.5/onecall"
+    relevant_data_categories = ["clouds", "visibility", "temp"]
 
     def __init__(self, location):
         self.location = location
     
     def fetch_weather_data(self):
-        payload = {"lat": self.location['lat'],"lon": self.location['lon'], "appid": OPEN_WEATHER_API_KEY}
+        payload = {"lat": self.location['lat'],"lon": self.location['lon'], "appid": OPEN_WEATHER_API_KEY, "units": "metric"}
         response = requests.get(self.base_url, params=payload)
         return response.json()
     
+    def extract_relevant_data(self, weather_data):
+        relevant_data = {}
+        relevant_data["current"] = self._extract_current_data(weather_data)
+        return relevant_data
+
+    def _extract_current_data(self, weather_data):
+        current_relevant_data = {}
+        for data_category in self.relevant_data_categories:
+            current_relevant_data[data_category] = weather_data["current"][data_category]
+        return current_relevant_data
