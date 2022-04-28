3c3a217db370f162c5455dfa6737bbf3ddc412a3
bmraubo <bmraubo@gmail.com>
2022-04-28 18:04:26 +0100

    ran black

--- server/weather_service/tests.py
+++ server/weather_service/tests.py
@@ -3,7 +3,6 @@ from weather_service.weather_service import WeatherService
 
 # Create your tests here.
 class TestWeatherService(TestCase):
-
     @classmethod
     def setUpTestData(cls):
         cls.location = {"lat": 10.00, "lon": 20.00}
@@ -19,6 +18,7 @@ class TestWeatherService(TestCase):
 
     def test_weather_service_can_extract_current_relevant_data(self):
         relevant_data = self.weather_service.extract_relevant_data(self.weather_data)
+        print(relevant_data)
 
         self.assertTrue(type(relevant_data) == dict)
         self.assertTrue("clouds" in relevant_data["current"].keys())
@@ -31,7 +31,9 @@ class TestWeatherService(TestCase):
         self.assertTrue("tonight" in relevant_data.keys())
         self.assertTrue("hourly" in relevant_data["tonight"].keys())
 
-    def test_weather_service_can_extract_correct_information_from_hourly_weather_data(self):
+    def test_weather_service_can_extract_correct_information_from_hourly_weather_data(
+        self,
+    ):
         relevant_data = self.weather_service.extract_relevant_data(self.weather_data)
 
         self.assertTrue("dt" in relevant_data["tonight"]["hourly"][0])
@@ -41,7 +43,9 @@ class TestWeatherService(TestCase):
         self.assertTrue("wind_speed" in relevant_data["tonight"]["hourly"][0])
         self.assertTrue("precipitation_chance" in relevant_data["tonight"]["hourly"][0])
 
-    def test_weather_service_can_extract_hourly_relevant_data_between_sunset_and_sunrise(self):
+    def test_weather_service_can_extract_hourly_relevant_data_between_sunset_and_sunrise(
+        self,
+    ):
         relevant_data = self.weather_service.extract_relevant_data(self.weather_data)
 
         first_hour_timestamp = relevant_data["tonight"]["hourly"][0]["dt"]
@@ -51,33 +55,33 @@ class TestWeatherService(TestCase):
         self.assertTrue(first_hour_timestamp > sunset)
         self.assertTrue(last_hour_timestamp < sunrise)
 
-
     def test_weather_service_can_extract_correct_sunrise_and_sunset(self):
         relevant_data = self.weather_service.extract_relevant_data(self.weather_data)
 
         self.assertTrue("sunset" in relevant_data["tonight"].keys())
         self.assertTrue("sunrise" in relevant_data["tonight"].keys())
-        self.assertTrue(relevant_data["tonight"]["sunrise"] > relevant_data["tonight"]["sunset"])
+        self.assertTrue(
+            relevant_data["tonight"]["sunrise"] > relevant_data["tonight"]["sunset"]
+        )
 
     def test_weather_service_can_extract_correct_moonrise_and_moonset(self):
         relevant_data = self.weather_service.extract_relevant_data(self.weather_data)
 
         self.assertTrue("moonrise" in relevant_data["tonight"].keys())
         self.assertTrue("moonset" in relevant_data["tonight"].keys())
-        self.assertTrue(relevant_data["tonight"]["moonset"] > relevant_data["tonight"]["moonrise"])
+        self.assertTrue(
+            relevant_data["tonight"]["moonset"] > relevant_data["tonight"]["moonrise"]
+        )
 
     def test_weather_service_can_convert_timestamp_to_date_time_format(self):
         timestamp = 1618315200
         expected_date_time = "13/04/2021 12:00"
-        
+
         date_time = self.weather_service._convert_time_format(timestamp)
 
         self.assertEqual(date_time, expected_date_time)
-    
+
     # def test_relevant_data_timestamps_have_been_converted(self):
     #     relevant_data = self.weather_service.extract_relevant_data(self.weather_data)
 
     #     self.assertTrue("/" and ":" in relevant_data["current"]["sunset"])
-
-
-    
--- server/weather_service/weather_service.py
+++ server/weather_service/weather_service.py
@@ -2,18 +2,24 @@ from server.main.settings import OPEN_WEATHER_API_KEY
 from datetime import datetime
 import requests
 
+
 class WeatherService:
     base_url = "https://api.openweathermap.org/data/2.5/onecall"
     relevant_data_categories = ["clouds", "temp"]
 
     def __init__(self, location):
         self.location = location
-    
+
     def fetch_weather_data(self):
-        payload = {"lat": self.location['lat'],"lon": self.location['lon'], "appid": OPEN_WEATHER_API_KEY, "units": "metric"}
+        payload = {
+            "lat": self.location["lat"],
+            "lon": self.location["lon"],
+            "appid": OPEN_WEATHER_API_KEY,
+            "units": "metric",
+        }
         response = requests.get(self.base_url, params=payload)
         return response.json()
-    
+
     def extract_relevant_data(self, weather_data):
         relevant_data = {}
         relevant_data["current"] = self._extract_current_data(weather_data["current"])
@@ -26,11 +32,17 @@ class WeatherService:
         current_weather["clouds"] = current_weather_data["clouds"]
         current_weather["weather"] = current_weather_data["weather"][0]["main"]
         return current_weather
-    
+
     def _extract_tonight_data(self, weather_data):
         tonight_relevant_data = {}
-        tonight_relevant_data = self._extract_basic_tonight_information(weather_data["daily"])
-        tonight_relevant_data["hourly"] = self._extract_hourly_tonight_information(weather_data["hourly"], tonight_relevant_data["sunset"], tonight_relevant_data["sunrise"])
+        tonight_relevant_data = self._extract_basic_tonight_information(
+            weather_data["daily"]
+        )
+        tonight_relevant_data["hourly"] = self._extract_hourly_tonight_information(
+            weather_data["hourly"],
+            tonight_relevant_data["sunset"],
+            tonight_relevant_data["sunrise"],
+        )
         return tonight_relevant_data
 
     def _extract_basic_tonight_information(self, daily_weather_data):
@@ -44,8 +56,10 @@ class WeatherService:
     def _extract_hourly_tonight_information(self, hourly_weather_data, sunset, sunrise):
         hourly_weather = []
         for hour in hourly_weather_data:
-            nighttime = self._is_after_sunset(hour["dt"], sunset) and self._is_before_sunrise(hour["dt"], sunrise)
-            if nighttime: 
+            is_nighttime = self._is_after_sunset(
+                hour["dt"], sunset
+            ) and self._is_before_sunrise(hour["dt"], sunrise)
+            if is_nighttime:
                 hour_weather = {}
                 hour_weather["dt"] = hour["dt"]
                 hour_weather["temp"] = hour["temp"]
@@ -55,13 +69,13 @@ class WeatherService:
                 hour_weather["precipitation_chance"] = hour["pop"]
                 hourly_weather.append(hour_weather)
         return hourly_weather
-    
+
     def _is_after_sunset(self, timestamp, sunset):
         return timestamp > sunset
 
     def _is_before_sunrise(self, timestamp, sunrise):
         return timestamp < sunrise
-    
+
     def _get_sunset(self, today_weather_data):
         return today_weather_data["sunset"]
 
